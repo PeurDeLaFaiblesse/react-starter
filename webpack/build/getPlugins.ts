ff69dc390d3f570b2d89fdd83f525f0cd800c513
Игорь Воропаев <ivoropaev@web-bee.ru>
2024-03-12 22:36:12 +0300

    feat: ts check dec

--- webpack/build/getPlugins.ts
+++ webpack/build/getPlugins.ts
@@ -3,20 +3,26 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import type { WebpackConfigOptions } from './types';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 export default ({ paths: { html }, isDev }: WebpackConfigOptions): WebpackPluginInstance[] => {
-  const plugins: WebpackPluginInstance[] = [new HtmlWebpackPlugin({ template: html })];
+  const plugins: WebpackPluginInstance[] = [
+    new HtmlWebpackPlugin({ template: html }),
+    new ForkTsCheckerWebpackPlugin(),
+  ];
 
   if (isDev) {
     plugins.push(new ProgressPlugin());
   } else {
     plugins.push(
-      new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-      }),
+      ...[
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[contenthash:8].css',
+          chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new BundleAnalyzerPlugin(),
+      ],
     );
-    plugins.push(new BundleAnalyzerPlugin());
   }
 
   return plugins;
