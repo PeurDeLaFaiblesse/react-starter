b64b8afa1484c983bb7c85d42c68230619eac683
Игорь Воропаев <ivoropaev@web-bee.ru>
2024-05-12 21:13:40 +0300

    chore: remove unused services

--- services/shop/src/bootstrap.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./global/cssReset.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "@/router";
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(<RouterProvider router={router} />);
